fce026bd3d8a9165f0b5498ad7b399214854f16c
tn18624 <tn18624@practise.com>
2025-05-17 09:56:24 +0000

    add more config

--- env.sh
+++ env.sh
@@ -2,6 +2,9 @@ export zookeeper_ip=localhost
 export broker1_ip=localhost
 export broker2_ip=10.10.10.12
 export broker3_ip=10.10.10.13
+export topic_send="transaction_input_1"
+export project_id="idontknow"
+export dataset="idontknow"
 
 export CONFLUENT_HOME=/opt/confluent-7.9.0
 export PATH=$PATH:$CONFLUENT_HOME/bin
